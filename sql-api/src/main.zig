4607033c29fd5c97d83eb2ddf23960470b719f7b
Gabriel Volpini <gabrielvolpini@192.168.0.9>
2026-03-11 19:45:30 -0300

    Init api

---
+++ sql-api/src/main.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+const zap = @import("zap");
+
+fn dispatch_routes(r: zap.Request) !void {
+    // dispatch
+    if (r.path) |the_path| {
+        if (routes.get(the_path)) |foo| {
+            try foo(r);
+            return;
+        }
+    }
+    // or default: present menu
+    try r.sendBody(
+        \\ <html>
+        \\   <body>
+        \\     <p><a href="/static">static</a></p>
+        \\     <p><a href="/dynamic">dynamic</a></p>
+        \\   </body>
+        \\ </html>
+    );
+}
+
+fn static_site(r: zap.Request) !void {
+    try r.sendBody("<html><body><h1>Hello from STATIC ZAP!</h1></body></html>");
+}
+
+var dynamic_counter: i32 = 0;
+fn dynamic_site(r: zap.Request) !void {
+    dynamic_counter += 1;
+    var buf: [128]u8 = undefined;
+    const filled_buf = try std.fmt.bufPrintZ(
+        &buf,
+        "<html><body><h1>Hello # {d} from DYNAMIC ZAP!!!</h1></body></html>",
+        .{dynamic_counter},
+    );
+    try r.sendBody(filled_buf);
+}
+
+fn setup_routes(a: std.mem.Allocator) !void {
+    routes = std.StringHashMap(zap.HttpRequestFn).init(a);
+    try routes.put("/static", static_site);
+    try routes.put("/dynamic", dynamic_site);
+}
+
+var routes: std.StringHashMap(zap.HttpRequestFn) = undefined;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    try setup_routes(allocator);
+
+    var listener = zap.HttpListener.init(.{
+        .port = 7776,
+        .on_request = dispatch_routes,
+        .log = true,
+    });
+    try listener.listen();
+
+    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
+
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
+}
